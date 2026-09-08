f0b03ffe176bfeb5550b84c8d9e03d5958090512
Akarsh-x64 <akarsh.sharma@hotmail.com>
2026-09-08 18:41:40 +0530

    testing

---
+++ modules/build/CMakeFiles/core.dir/link.d
@@ -0,0 +1,115 @@
+core.cpython-314-x86_64-linux-gnu.so: \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crti.o \
+  /usr/lib/gcc/x86_64-redhat-linux/16/crtbeginS.o \
+  CMakeFiles/core.dir/bindings.cpp.o \
+  CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/OS/Linux/LinuxStorageDevice.cpp.o \
+  CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/Hardware/Magnetic/HDDController.cpp.o \
+  CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/File_Systems/exFAT/exFAT.cpp.o \
+  CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/File_Systems/ext4/ext4.cpp.o \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libstdc++.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so \
+  /lib64/libm.so.6 \
+  /lib64/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so \
+  /usr/lib64/libatomic.so.1.2.0 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so \
+  /lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/16/crtendS.o \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crtn.o
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crti.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/crtbeginS.o:
+
+CMakeFiles/core.dir/bindings.cpp.o:
+
+CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/OS/Linux/LinuxStorageDevice.cpp.o:
+
+CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/Hardware/Magnetic/HDDController.cpp.o:
+
+CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/File_Systems/exFAT/exFAT.cpp.o:
+
+CMakeFiles/core.dir/home/ak/dev/dataEraser/Secure-data-erasure-and-recovery/Erasure/File_Systems/ext4/ext4.cpp.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libstdc++.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so:
+
+/lib64/libm.so.6:
+
+/lib64/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so:
+
+/usr/lib64/libatomic.so.1.2.0:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so:
+
+/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/crtendS.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crtn.o:
